e4a9e7b7292756dcf16b4da7f9b96d2048acc24e
Fahadbin Mihad Alam <121739350+Fahadbin-Alam@users.noreply.github.com>
2026-02-14 09:32:15 -0500

    edite

--- helloworl.py
+++ helloworl.py
@@ -6,3 +6,22 @@ def add_numbers(a,b):
 
 def numbers(x,y)
     return add_numbers(x,y)
+
+name = fahadbin
+age = 23
+
+#snakecase
+my_variable_name = 'Retarded Dude'
+
+#user_age is better than age or an abbreivation like ua.
+user_age = 45
+
+#please for the love of god do not use single letter variables like
+x = 56 #like wtf does x mean?
+
+print('Hello you fucken retards') #hello world shit
+print ('Hell0', 'you','fucken','retards') #same thing as line 22
+
+#unlike other code Python is a bit faster
+
+
