766a3cd443509e5b3633855a1b286ef152c712a5
Fahadbin Mihad Alam <121739350+Fahadbin-Alam@users.noreply.github.com>
2026-02-19 13:41:29 -0500

    edited

--- helloworl.py
+++ helloworl.py
@@ -24,4 +24,71 @@ print ('Hell0', 'you','fucken','retards') #same thing as line 22
 
 #unlike other code Python is a bit faster
 
+#variables
+#Learn how memory works
+hello = 'tim'
+world = 'world'
+world = hello
+hello = 'no'
+#even though hello is equal to no now. World is still equal to tim.
 
+hello_world #use underscore for space and use lowercase for variables (you can do capital but just don't lol)
+
+#Input
+input('Name:')
+
+name = input("Name:")
+age = input("Age:")
+print('hello',name,'you are', age, 'years old')
+
+#arithmatic 
+x = 9
+y = 3
+result = (x * y)
+print(result)
+# + - / * and ** for exponent and // gives us the integer of 3.33 so it would just be 3 if it was 10/3. % gives us the remainder 
+b - bracket
+e - exponent
+d - division
+m - multiplication
+a - Addition
+s - subtraction
+
+num = input("Number: ")
+print(int(num) * 2) # so whatever even though it's a string int() will automatically turn it into a integer.
+
+#string Method: the cool thing is you can chain the together.
+hello = 'hello' .upper()
+print(hello) #prints hello in all caps HELLO
+# you can also do print(hello.upper)
+#same thing with .lower 
+#you use this with user inputs
+#.capitalize just caps the first letter. Actually it fixes the grammar
+#.count it will count the amount of letters in the string.
+    hello = 'hello world'
+    print(hello.count('ll)')) #it will bring out 2 but if it was caps it wouldn't. to fix this you can do
+                                                    print(hello.lower().count('ll'))
+
+#string multiplication
+x = 'hello'
+y = 3
+print(x*y) #hello hello hello
+#string concatination
+x = 'hell'
+y = 'o'
+print(x + y) #hello
+
+#Conditional Operators
+#compares two variables and gives yes or no or true or false
+== left equal to right?
+!= inequality; left is not equal to right
+<=
+>=
+<
+>
+
+x = "Hello"
+y = 'Hello'
+
+print (x==y) # true
+print (x!=y) #false
